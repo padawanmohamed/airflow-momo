8ce0bcdd43b768be7bec534aee51faec8f858bd4
PadawanMomo <41162519+padawanmohamed@users.noreply.github.com>
2026-07-23 13:20:59 +0200

    Update dag-rami.py

--- dag-rami.py
+++ dag-rami.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 with DAG(
-    dag_id="dag_formation",
+    dag_id="dag_formation_INCROYABLEQUIFAITHELLO",
     start_date=datetime(2025, 1, 1),
     schedule=None,
     catchup=False,

e403797f016913c50483709e82dea81c88ab0154
PadawanMomo <41162519+padawanmohamed@users.noreply.github.com>
2026-07-23 14:35:31 +0200

    Update dag-rami.py

--- dag-rami.py
+++ dag-rami.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from airflow import DAG
+from airflow.utils.trigger_rule import TriggerRule
 from airflow.operators.bash import BashOperator
 with DAG(
     dag_id="dag_formation_INCROYABLEQUIFAITHELLO",
@@ -8,16 +9,22 @@ with DAG(
     catchup=False,
     tags=["guide"],
 ) as dag:
-    hello = BashOperator(
-        task_id="hello",
+    task1 = BashOperator(
+        task_id="task1",
         bash_command="echo 'Airflow est prêt sur cette EC2'; hostname; date"
     )
-    liste = BashOperator(
-        task_id="liste",
-        bash_command="ls -la"
+    task2 = BashOperator(
+        task_id="task2",
+        bash_command="wesh"
     )
-    show_hostname = BashOperator(
-        task_id="show_hostname",
+    task3 = BashOperator(
+        task_id="task3",
         bash_command="hostname"
+        trigger_rule=TriggerRule.ONE_SUCCESS
     )
-    hello >> liste >> show_hostname
+    task4 = BashOperator(
+        task_id="exitone",
+        bash_command="exit 1"
+        trigger_rule=TriggerRule.ALL_FAILED
+    )
+    [task1, task2] >> [task3, task4]
